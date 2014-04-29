8816cd497073815897f0a73fd8bef03dd53c3de4
polyturn <kleun56@yahoo.com>
2014-04-28 22:25:22 -0400

    init commit

---
+++ govnuMvc5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(govnuMvc5.Startup))]
+namespace govnuMvc5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
